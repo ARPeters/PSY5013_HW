3e530823a2cdbb449222ea47f77c17b30e6444d5
ARPeters <AndrewPeters@Jane.local>
2015-02-15 21:45:33 -0600

    Finished second-to-last set of questions

--- PSY5013_HW2.R
+++ PSY5013_HW2.R
@@ -1,8 +1,14 @@
 library(foreign)
 library(psych)
 library(influence.ME)
-install.packages("lme4")
 library(lme4)
+library(car)
+library(qqnorm)
+library(het.test)
+install.packages("nortest")
+library(nortest)
+install.packages("bstats")
+library(het.test)
 ds<-read.csv("gpa.csv")
 head(ds)
 colnames(ds)
@@ -47,10 +53,81 @@ hv$warn<-ifelse(hv[,'hatvalues(model1)']>3*mn, "x3",
                 ifelse(hv[,'hatvalues(model1)']>2*mn, "x2", 0))
 hv
 
-cbind(ds, hv)
+dsNew<-cbind(ds, hv)
 
-plot(hatvalues(model1, type=""))
-plot(model1)
+#Individuals with high leverage:
+dsHighL<-dsNew[dsNew$warn!=0,]
+
+#Leverage for reversed model
+#model2<-lm(SAT~GPA, data=ds)
+#hatvalues(model2)
+
+#hv2<-as.data.frame(hatvalues(model2))
+#mn2<-mean(hatvalues(model2))
+
+#hv2$warn<-ifelse(hv2[,'hatvalues(model2)']>3*mn2, "x3", 
+#                ifelse(hv2[,'hatvalues(model2)']>2*mn2, "x2", 0))
+
+#dsNew2<-cbind(ds, hv2)
+
+#Individuals with high leverage:
+#dsHighL2<-dsNew2[dsNew2$warn!=0,]
 
 
+#Individuals with high cook's d
+#Note: High Cook's D = 4/(N-K-1) = (4/(250-1-1))=4/248 = 0.01612903
 
+influenceObject<-(model1)
+cooks.distance(influenceObject)
+
+cooksD<-as.data.frame(cooks.distance(influenceObject))
+colnames(cooksD)<-c("Distance")
+cooksD$warnD<-ifelse(cooksD$Distance>(4/248), "High", 0)
+
+dsNew<-cbind(dsNew, cooksD)
+dsCooksDHigh<-dsNew[dsNew$warnD!=0,]
+
+#Studentized residuals
+rstudent(model1)
+rStudentized<-as.data.frame(rstudent(model1))
+colnames(rStudentized)<-c("warnRstud")
+dsNew<-cbind(dsNew, rStudentized)
+dsNew$warnRstud<-ifelse(rStudentized>=1.96, "High", ifelse(rStudentized<=-1.96, "Low", 0))
+dsNew$warnRstud<-as.data.frame(dsNew$warnRstud)
+dsRstudWarn<-dsNew[dsNew$warnRstud!=0,]
+
+colnames(dsNew)<-c("SAT", "GPA", "Prevach", "Highach", "GPAHat", "warnLeverage", "Distance", "warnD", "StudRes", "warnRstud")
+colnames(dsRstudWarn)<-c("SAT", "GPA", "Prevach", "Highach", "GPAHat", "warnLeverage", "Distance", "warnD", "StudRes", "warnRstud")
+
+dsRstudWarn
+
+dsFinal<-dsNew[dsNew$warnLeverage!=0 | dsNew$warnD!=0 | dsNew$warnRstud!=0,]
+dsFinal
+colnames(dsFinal)
+dsFinal2<-dsFinal[,c(1,2,3,6,8,10)]
+colnames(dsFinal2)
+
+#With "outliers" removed.
+dsNeutered<-dsNew[dsNew$warnD==0 | dsNew$warnRstud==0, ]
+
+model1<-lm(GPA~SAT, data=ds)
+modelNeutered<-lm(dsNeutered$GPA~dsNeutered$SAT)
+
+summary(model1)
+summary(modelNeutered)
+
+#Normality of errors
+StudentizedResiduals<-rstudent(model1)
+hist(StudentizedResiduals)
+str(shapiro.test(GPAonSAT$residuals))
+
+#Homoskedasticity of erros
+#White's test
+GPAonSAT<-model1
+spreadLevelPlot(GPAonSAT)
+
+GPAonSATnew<-lm(GPA~SAT+(SAT*SAT), data=ds)
+
+whites.htest(model1)
+#Independence of one residual on preceding observation
+durbinWatsonTest(GPAonSAT)
